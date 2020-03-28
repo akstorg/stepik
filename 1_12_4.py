cdcadfd9a1f176c3949ee526657974bf6a3ec1ea
Alex <akstorg+git@gmail.com>
2020-03-28 21:02:58 +0200

    треугольник - нужно проверить

--- 1_12_4.py
+++ 1_12_4.py
@@ -8,3 +8,14 @@ if fig == 'круг':
     p = 3.14 * r**2
     print(p)
 
+
+
+if fig == 'треугольник':
+    print('enter a')
+    a = int(input())
+    print('enter b')
+    b= int(input())
+    print('enter c')
+    c = int(input())
+    p = sum([a, b, c]) / 2
+    print((p * (p - a) * (p - b) * (p - c)) ** 0.5)
